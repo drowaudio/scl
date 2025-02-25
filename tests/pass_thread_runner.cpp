bdfcc896e43b9efa6286a63e44a85bfd39910724
David Rowland <drowland96@gmail.com>
2025-02-25 14:38:51 +0000

    Moved the reflection version of send to the same file as the type-trait one and prefer it if reflection is available

--- tests/pass_thread_runner.cpp
+++ tests/pass_thread_runner.cpp
@@ -3,6 +3,41 @@
 #include <print>
 #include <scl/safe_thread.h>
 
+/**
+    A wrapper that can be used around pointers to allow the this pointer to be passed.
+    There's nothing special about this, it's just a dump pointer but means we can
+    explicitly allow it when we get reflection and makes it obvious in calling
+    code that the intent is to share it.
+ */
+template<typename T>
+class unchecked_pointer
+{
+public:
+    unchecked_pointer (nullptr_t) = delete;
+
+    unchecked_pointer (T* p)
+        : pointer (p) {
+    }
+
+    unchecked_pointer (T& p)
+        : pointer (*p) {
+    }
+
+    T& operator*() const {
+        return *pointer;
+    }
+
+    T* operator->() const {
+        return pointer;
+    }
+
+private:
+    T* pointer;
+};
+
+template<typename T>
+struct scl::is_send<unchecked_pointer<T>> : std::true_type {};
+
 using namespace std::literals;
 
 class thread_runner
@@ -10,11 +45,11 @@ class thread_runner
 public:
     thread_runner()
     {
-        std::mem_fn (&thread_runner::run) (scl::unchecked_pointer<thread_runner> (this));
+        std::mem_fn (&thread_runner::run) (unchecked_pointer<thread_runner> (this));
 
         // static_assert(is_send_v<decltype(std::mem_fn (&thread_runner::run))>);
         // static_assert(is_send_v<std::decay_t<decltype(std::mem_fn (&thread_runner::run))>>);
-        thread = std::make_unique<scl::thread> (std::mem_fn (&thread_runner::run), scl::unchecked_pointer (this));
+        thread = std::make_unique<scl::thread> (std::mem_fn (&thread_runner::run), unchecked_pointer (this));
         // thread = std::make_unique<safe_thread> (std::mem_fn (&thread_runner::run), this_wrapper<const thread_runner> (const_cast<const thread_runner*> (this)));
         // thread = std::make_unique<safe_thread> (std::mem_fn (&thread_runner::run), const_cast<const thread_runner*> (this));
     }
